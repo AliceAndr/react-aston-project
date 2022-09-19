486a7a721dbe17e19243e650058a8b3e7ac693dc
Alice <andreevaalisa42@gmail.com>
2022-09-19 18:37:17 +0300

    fix mistakes from the latest PR

--- src/pages/HousePage/HousePage.tsx
+++ src/pages/HousePage/HousePage.tsx
@@ -13,7 +13,7 @@ export const HousePage = () => {
   const { name: paramsName = '' } = useParams();
   const { data, isLoading } = useGetOneHouseQuery(paramsName);
   const user = useCurrentUser();
-  const isInFavorite = user?.favorites?.find((el: { name: string, url: string }) => el.name === paramsName);
+  const isInFavorite = user?.favorites?.find((el) => el.name === paramsName);
 
   const toggleFavorites = () => {
     dispatch(toggleFavorite({ name: data?.name, url: window.location.href }))
@@ -25,36 +25,36 @@ export const HousePage = () => {
         <Loader />
       </div>
     )
-  } else {
-    return (
-      <div className="app__bookpage">
-        <h1>House Info: </h1>
-        <div className="app__bookpage-infoWrap">
-          <div className="app__bookpage-infoWrap-info"><span>Name: </span>{data?.name}</div>
-          <div className="app__bookpage-infoWrap-info"><span>Words: </span>{data?.words || 'No info'} </div>
-          <div className="app__bookpage-infoWrap-info"><span>Region: </span>{data?.region || 'No info'}</div>
-          <div className="app__bookpage-infoWrap-info"><span>Coat of Arms: </span>{data?.coatOfArms || 'No info'}</div>
-          <div className="app__bookpage-infoWrap-info"><span>The Great Title: </span>{data?.titles?.join(', ') || 'No info'}  </div>
-          <div className="app__bookpage-infoWrap-info"><span>Seats: </span>{data?.seats.join(', ') || 'No info'} </div>
-          <div className="app__bookpage-infoWrap-info"><span>Died out: </span>{data?.diedOut || 'Still Alive'} </div>
-
-          {
-            user?.email ? (
-              isInFavorite ?
-                <div className="app_favorite-button" onClick={toggleFavorites}>
-                  <p className="app_favorite-p">Delete from Favorites</p>
-                  <FavoriteIcon />
-                </div>
-                :
-                <div className="app_favorite-button" onClick={toggleFavorites}>
-                  <p className="app_favorite-p">Add to Favorites</p>
-                  <FavoriteBorderIcon />
-                </div>
-            ) : null
-          }
-
-        </div>
-      </div>
-    )
   }
+
+  return (
+    <div className="app__bookpage">
+      <h1>House Info: </h1>
+      <div className="app__bookpage-infoWrap">
+        <div className="app__bookpage-infoWrap-info"><span>Name: </span>{data?.name}</div>
+        <div className="app__bookpage-infoWrap-info"><span>Words: </span>{data?.words || 'No info'} </div>
+        <div className="app__bookpage-infoWrap-info"><span>Region: </span>{data?.region || 'No info'}</div>
+        <div className="app__bookpage-infoWrap-info"><span>Coat of Arms: </span>{data?.coatOfArms || 'No info'}</div>
+        <div className="app__bookpage-infoWrap-info"><span>The Great Title: </span>{data?.titles?.join(', ') || 'No info'}  </div>
+        <div className="app__bookpage-infoWrap-info"><span>Seats: </span>{data?.seats.join(', ') || 'No info'} </div>
+        <div className="app__bookpage-infoWrap-info"><span>Died out: </span>{data?.diedOut || 'Still Alive'} </div>
+
+        {
+          user?.email ? (
+            isInFavorite ?
+              <div className="app_favorite-button" onClick={toggleFavorites}>
+                <p className="app_favorite-p">Delete from Favorites</p>
+                <FavoriteIcon />
+              </div>
+              :
+              <div className="app_favorite-button" onClick={toggleFavorites}>
+                <p className="app_favorite-p">Add to Favorites</p>
+                <FavoriteBorderIcon />
+              </div>
+          ) : null
+        }
+
+      </div>
+    </div>
+  )
 }
